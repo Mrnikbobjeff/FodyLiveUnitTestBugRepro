3668a83a8e1928cab86088c0e825ec36af900a70
Mrnikbobjeff <schillinik@yahoo.de>
2018-07-02 18:33:13 +0200

    added minimal repro for fody bug

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+
+namespace ClassLibrary1
+{
+    public class Class1 : ReactiveObject
+    {
+        [Reactive]
+        public bool BoolToChange { get; set; }
+
+        public bool Inverse { get; set; }
+        public Class1()
+        {
+            this.WhenAnyValue(x => x.BoolToChange).Subscribe(val => Inverse = !val);
+        }
+    }
+}
---
+++ ClassLibrary1/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary1;
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var c = new Class1();
+            c.BoolToChange = true;
+            Assert.False(c.Inverse);
+        }
+    }
+}
